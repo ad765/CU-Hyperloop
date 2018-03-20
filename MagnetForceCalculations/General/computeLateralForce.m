8125907b1625df9be916ec81d738d3388cf542a4
jaehyungjoo <32003815+jj534@users.noreply.github.com>
2018-03-20 10:37:48 -0400

    Magnet lateral force calculation script

---
+++ MagnetForceCalculations/General/computeLateralForce.m
@@ -0,0 +1,21 @@
+function [lateralForce] = computeLateralForce(airGap)
+
+%i-beam constants
+alumResistivity = 3.99e-006; %ohm-cm
+alumResistivity = (100)*alumResistivity; %ohm-m
+alumConducitivity = 1/alumResistivity; %S/m
+iBeamThick = 0.313*(0.0254); %meters
+%relative permeability
+mu = 4*pi*10^(-7)*200000;
+
+podVelocity = 200*(0.44704); %meters per second
+
+%constants chosen for simulation purposes
+windings = 1000;
+current = 1;
+crossSectArea = 1;
+
+%compute lateral force for one magnet using magnetLateralForce Module
+[lateralForce] = magnetLateralForce(alumConducitivity, ...
+mu, iBeamThick, podVelocity, airGap, windings, current, crossSectArea);
+end
